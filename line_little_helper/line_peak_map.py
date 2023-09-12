90ab9cc35a637c7cdb6ad92459c6540482627881
Fernando Olguin <f.olguin.ch@gmail.com>
2023-09-12 09:12:52 +0800

    Create a varying window based on pixel linewidth

--- line_little_helper/line_peak_map.py
+++ line_little_helper/line_peak_map.py
@@ -14,6 +14,7 @@ from toolkit.astro_tools import cube_utils
 import astropy.units as u
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.ndimage as ndimg
 
 from line_little_helper import argparse_parents
 from line_little_helper import argparse_processing as processing
@@ -34,7 +35,10 @@ def save_map(data: u.Quantity,
       suffix: file suffix.
     """
     # Header
-    header = cube.wcs.sub(['longitude', 'latitude']).to_header()
+    if data.ndim == 2:
+        header = cube.wcs.sub(['longitude', 'latitude']).to_header()
+    elif data.ndim == 3:
+        header = cube.wcs.to_header()
     if hasattr(data, 'unit'):
         header['BUNIT'] = f'{data.unit:FITS}'
 
@@ -135,7 +139,7 @@ def _proc(args: argparse.Namespace):
         args.linefreq = transition.restfreq
 
         # Get channel range
-        processing.get_channel_range(args)
+        processing.get_channel_range(args, allow_all=True)
 
         # Spectral axis
         equiv = u.doppler_radio(args.linefreq)
@@ -144,7 +148,10 @@ def _proc(args: argparse.Namespace):
 
         # Select channels
         ind = np.squeeze(np.zeros(vel_axis.shape, dtype=bool))
+        args.log.info("Channel range: %i - %i", args.chan_range[0],
+                      args.chan_range[1])
         ind[args.chan_range[0]:args.chan_range[1] + 1] = True
+        valid_chans = np.sum(ind)
         masked_cube = args.cube.mask_channels(ind)
         args.log.info('Cube size: %i', args.cube.size)
         args.log.info('Valid data after spectral range: %i',
@@ -155,27 +162,86 @@ def _proc(args: argparse.Namespace):
         fwhm_mask = masked_cube.filled_data[:] > 0.5 * data_max_map
         fwhm_map_chan = np.sum(fwhm_mask, axis=0)
         fwhm_map_chan = fwhm_map_chan.astype(float)
+        args.log.info('Data over FWHM: %i', np.sum(fwhm_mask))
 
         # Threshold mask
         args.log.info('Flux limit: %f %s', args.flux_limit[0].value,
                       args.flux_limit[0].unit)
-        masked_cube = masked_cube.with_mask(masked_cube > args.flux_limit[0])
+        threshold_mask = masked_cube > args.flux_limit[0]
+        threshold_cube = masked_cube.with_mask(threshold_mask)
         args.log.info('Valid data after threshold mask: %i',
-                      np.sum(masked_cube.mask.include()))
+                      np.sum(threshold_cube.mask.include()))
 
-        # Max map
-        mask_bad = np.all(np.isnan(masked_cube.filled_data[:]), axis=0)
-        masked_cube = masked_cube.with_fill_value(-np.inf)
-        ind_map = np.nanargmax(masked_cube.filled_data[:], axis=0)
+        # Max velocity map
+        mask_bad = np.all(np.isnan(threshold_cube.filled_data[:]), axis=0)
+        threshold_cube = threshold_cube.with_fill_value(-np.inf)
+        ind_map = np.nanargmax(threshold_cube.filled_data[:], axis=0)
         vel_map = vel_axis[ind_map]
         vel_map[mask_bad] = np.nan
 
-        # FWHM in velocity
+        # Channel width
         med_chan = int(np.median(ind_map[~mask_bad]))
         args.log.info('Median channel: %i', med_chan)
         chan_width = np.abs(vel_axis[:-1] - vel_axis[1:])
         chan_width = chan_width[med_chan]
         args.log.info('Channel width: %f %s', chan_width.value, chan_width.unit)
+
+        # Make a mask from fwhm 
+        suffix = transition.generate_name()
+        if args.nlinewidth:
+            args.log.info('Dilating mask by %i x linewidth', args.nlinewidth)
+
+            # Use input linewidth map
+            if args.linewidth_map:
+                args.log.info('Using input linewidth map for mask')
+                lw_map = fits.open(args.linewidth_map)[0]
+                lw_map_chan = lw_map.data * u.Unit(lw_map.header['BUNIT'])
+                lw_map_chan = lw_map_chan.to(chan_width.unit) / chan_width
+                lw_map_chan = lw_map_chan.to(1).value
+            else:
+                lw_map_chan = fwhm_map_chan
+            # We use fmax to avoid rounding errors when lw_map_chan/2 < 1
+            dilate_by = np.fmax(lw_map_chan * args.nlinewidth / 2, 1)
+            dilate_by = dilate_by.astype(int)
+            nfwhm_mask = np.zeros(threshold_cube.shape, dtype=bool)
+            m, n = ind_map.shape
+            I, J = np.ogrid[:m,:n]
+            nfwhm_mask[ind_map, I, J] = True
+            structure = np.array(
+                [[[0, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 0]],
+                 [[0, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 0]],
+                 [[0, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 0]]],
+                dtype=bool,
+            )
+            invalid = np.all(~threshold_mask.include(), axis=0)
+            for niter in np.unique(dilate_by):
+                if niter == 0:
+                    continue
+                aux = np.zeros(threshold_cube.shape, dtype=bool)
+                aux[ind_map, I, J] = True
+                aux[:, dilate_by != niter] = False
+                aux[:, invalid.astype(bool)] = False
+                aux = ndimg.binary_dilation(aux, structure=structure,
+                                            iterations=niter)
+                nfwhm_mask = (nfwhm_mask | aux)
+            nfwhm_mask[:, invalid] = True
+            args.log.info('Valid data after FWHM dilation: %i',
+                          np.sum(nfwhm_mask))
+
+            # Save mask
+            save_map(nfwhm_mask.astype(int) * u.Unit(1), args.cube,
+                     args.cubename[0],
+                     args.outdir[0], f'{suffix}_fwhm_mask')
+        else:
+            nfwhm_mask = None
+
+        # FWHM in velocity
         fwhm_map = fwhm_map_chan * chan_width
 
         # Apply mask
@@ -185,7 +251,6 @@ def _proc(args: argparse.Namespace):
         ind_map[mask_bad] = np.nan
 
         # Save maps
-        suffix = transition.generate_name()
         save_map(ind_map * u.Unit(1), args.cube, args.cubename[0],
                  args.outdir[0], f'{suffix}_peakchannel')
         save_map(vel_map, args.cube, args.cubename[0], args.outdir[0],
@@ -198,8 +263,12 @@ def _proc(args: argparse.Namespace):
         # Calculate moments
         if args.moments is not None:
             #fwhm_mask[mask_bad] = False
-            args.log.info('FWHM mask valid data: %i', np.sum(fwhm_mask))
-            moment_cube = masked_cube.with_mask(fwhm_mask)
+            #args.log.info('FWHM mask valid data: %i', np.sum(fwhm_mask))
+            #fwhm_mask[:][np.all(~threshold_mask, axis=0)] = 
+            if nfwhm_mask is not None:
+                moment_cube = masked_cube.with_mask(nfwhm_mask)
+            else:
+                moment_cube = masked_cube.with_mask(fwhm_mask)
             args.log.info('Valid data for moment: %i',
                           np.sum(moment_cube.mask.include()))
             masked_chans = np.any(fwhm_mask, axis=(1,2))
@@ -209,14 +278,19 @@ def _proc(args: argparse.Namespace):
             #moment_cube = cube_utils.to_common_beam(moment_cube,
             #                                        log=args.log.info)
             for moment in args.moments:
-                filename = f'{args.cubename[0].stem}_{suffix}_peak_moment{moment}.fits'
+                filename = f'{args.cubename[0].stem}_{suffix}_'
+                if args.nlinewidth:
+                    filename += f'{args.nlinewidth}xlinewidth_'
+                else:
+                    filename += f'peak_'
+                filename += f'moment{moment}.fits'
                 filename = args.outdir[0] / filename
                 cube_utils.get_moment(moment_cube,
                                       moment,
                                       linefreq=args.linefreq,
                                       lower_limit=args.flux_limit[0],
-                                      #rms=args.rms,
-                                      auto_rms=True,
+                                      rms=args.rms,
+                                      #auto_rms=True,
                                       nsigma=args.nsigma[0],
                                       filename=filename,
                                       log=args.log.info)
@@ -256,6 +330,11 @@ def line_peak_map(args: Optional[Sequence[str]] = None) -> None:
         conflict_handler='resolve')
     parser.add_argument('--fit_halfwidth', nargs=1, type=int, default=[None],
                         help='Half window channel size for fitting')
+    parser.add_argument('--nlinewidth', type=int, default=None,
+                        help='Time the FWHM mask is dilated')
+    parser.add_argument('--linewidth_map', default=None,
+                        action=actions.CheckFile,
+                        help='Line width map to determine integration window')
     parser.add_argument('--moments', nargs='*', type=int,
                         help='Moments to calculate')
     parser.add_argument('outdir', nargs=1, action=actions.NormalizePath,
